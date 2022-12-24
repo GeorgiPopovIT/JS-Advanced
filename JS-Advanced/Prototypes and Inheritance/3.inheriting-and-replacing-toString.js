68ab663d913a0c0f394602f3b5cee0f779a1a503
GeorgiPopovIT <gogopopo262003@gmail.com>
2022-12-24 15:50:22 +0200

    Added 03. inherit and replacing toString.

---
+++ JS-Advanced/Prototypes and Inheritance/3.inheriting-and-replacing-toString.js
@@ -0,0 +1,47 @@
+function toStringExtension(){
+    class Person{
+        constructor(name, email){
+            this.name = name;
+            this.email = email;
+        }
+        toString(){
+            return `${this.constructor.name} (name: ${this.name}, email: ${this.email})`;
+        }
+    }
+    class Teacher extends Person{
+        constructor(name, email, subject){
+            super(name,email);
+            this.subject = subject;
+        }
+
+        toString(){
+          return super.toString() + `, subject: ${this.subject})`;
+        }
+    }
+    class Student extends Person{
+        constructor(name, email, course){
+            super(name,email);
+            this.course = course;
+        }
+
+        toString(){
+            return super.toString() + `, course: ${this.course})`;
+          }
+    }
+
+    return {
+        Person,
+        Teacher,
+        Student
+    };
+}
+
+let classes = toStringExtension();
+let Person = classes.Person;
+let Teacher = classes.Teacher;
+let Student = classes.Student;
+
+let p = new Person("Pesho",'Pesho@pesh.com');
+let t = new Teacher("Ivan",'ivan@ivan.com',"Graphics");
+console.log(p.toString());
+console.log(t.toString());
