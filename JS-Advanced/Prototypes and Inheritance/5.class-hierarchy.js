a261f5cef8e0013ca2a93b911572a6c03548cf7f
GeorgiPopovIT <gogopopo262003@gmail.com>
2022-12-25 00:25:10 +0200

    Add 05.Class- hierarchy

---
+++ JS-Advanced/Prototypes and Inheritance/5.class-hierarchy.js
@@ -0,0 +1,78 @@
+class Figure{
+    constructor(units = 'cm'){
+        this.units = units;
+    }
+
+    changeUnits(units){
+        this.units = units;
+    }
+    scaleParam(value){
+        if(this.units == 'm'){
+            value /= 10;
+        }
+        else if(this.units == 'mm'){
+            value *= 10;
+        }
+
+        return value;
+    }
+
+    toString(){
+        return `Figures units: ${this.units}`;
+    }
+}
+
+class Circle extends Figure{
+    constructor(radius, units) {
+        super(units);
+        this._radius = radius;
+    }
+
+    get area(){
+        return Math.PI * this.radius * this.radius;
+    }
+
+    get radius(){
+       return this.scaleParam(this._radius);
+    }
+    
+    toString(){
+        return super.toString() + ` Area: ${this.area} - radius: ${this.radius}`;
+    }
+}
+
+class Rectangle extends Figure{
+    constructor(width, height, units){
+        super(units);
+        this._width = width;
+        this._height = height;
+    }
+
+    get area(){
+        return this.width * this.height;
+    }
+
+    get height(){
+        return this.scaleParam(this._height);
+    }
+
+    get width(){
+        return this.scaleParam(this._width);
+    }
+
+    toString(){
+        return super.toString() + ` Area: ${this.area} - width: ${this.width}, height: ${this.height}`;
+    }
+    
+}
+
+
+let c = new Circle(5);
+console.log(c.area); // 78.53981633974483
+console.log(c.toString()); // Figures units: cm Area: 78.53981633974483 - radius: 5
+let r = new Rectangle(3, 4, 'mm');
+console.log(r.area); // 1200
+console.log(r.toString()); //Figures units: mm Area: 1200 - width: 30, height: 40
+r.changeUnits('cm');
+console.log(r.area);
+console.log(r.toString()); // Figures units: cm Area: 12 - width: 3, height: 4 c.changeUnits('mm'); console.log(c.area); // 7853.981633974483 console.log(c.toString())
